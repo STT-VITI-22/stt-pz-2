13a4e205313a8f1a57713842c055c2a68e558f14
N9verMore <ilya.tsyunyak@gmail.com>
2025-09-09 17:58:20 +0300

    moved lib.spec.js to tests package. Wrote tests according to TDD

--- js/lib.spec.js
+++
@@ -1,9 +0,0 @@
-const lib = require('./lib');
-
-describe('Test suite for testing lib.js', () => {
-  describe('Test suite for testing subtraction function', () => {
-    it('should return subtraction of two numbers', () => {
-      expect(lib.subtraction(5, 1)).toEqual(3);
-    });
-  });
-});
