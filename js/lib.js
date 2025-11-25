58933f084f364a8e1ce8a776f000dab7afc351ba
narcissus204 <kirilkreul@gmail.com>
2025-11-25 11:47:58 +0200

    finish

--- js/lib.js
+++ js/lib.js
@@ -1,134 +1,112 @@
 /**
+ * This function must add two numbers and return result of addition
+ */
+function sum(a, b) {
+  return Number(a) + Number(b);
+}
+
+/**
  * This function must subtraction two numbers and return result of subtraction
- * @param a {number|string}
- * @param b {number|string}
- * @returns {number}
  */
 function subtraction(a, b) {
-  // Write your code here
+  return Number(a) - Number(b);
 }
 
 /**
  * This function must division two numbers and return result according to all arithmetic rules
- * @param a {number|string}
- * @param b {number|string}
- * @returns {number}
  */
 function division(a, b) {
-  // Write your code here
+  if (Number(b) === 0) throw new Error("Division by zero");
+  return Number(a) / Number(b);
 }
 
 /**
  * This function return result of multiplication two numbers according to all arithmetic rules
- * @param a {number|string}
- * @param b {number|string}
- * @returns {number}
  */
 function multiplication(a, b) {
-  // Write your code here
+  return Number(a) * Number(b);
 }
 
 /**
  * This function calculate percent from number according to all arithmetic rules
- * @param value {number|string}
- * @param percent {number|string}
- * @returns {number}
  */
 function percentage(value, percent) {
-  // Write your code here
+  return (Number(value) * Number(percent)) / 100;
 }
 
 /**
  * This function return whole part of number
- * @param number {number|string}
- * @returns {number}
  */
 function getWholeNumberPart(number) {
-  // Write your code here
+  return Math.floor(Number(number));
 }
 
 /**
  * This function get array of natural number and return array of even number
- * @param numbers {number[]}
- * @returns {number[]}
  */
 function getEvenNumbers(numbers) {
-  // Write your code here
+  return numbers.filter(n => n % 2 === 0);
 }
 
 /**
- * This function get get Max number from array
- * @param numbers {number[]}
- * @returns {number}
+ * This function get Max number from array
  */
 function getMaxNumber(numbers) {
-  // Write your code here
+  return Math.max(...numbers);
 }
 
 /**
- * This function return object with min and  max number from array
- * @param numbers {number[]}
- * @returns {{min: number, max: number}}
+ * This function return object with min and max number from array
  */
 function getMinAndMaxNumbers(numbers) {
-  // Write your code here
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  };
 }
 
 /**
  * This function calculate arithmetic means from array of numbers
- * @param numbers {number[]}
- * @returns {number}
  */
 function arithmeticMeans(numbers) {
-  // Write your code here
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((acc, val) => acc + val, 0);
+  return sum / numbers.length;
 }
 
 /**
- * This function return array of vowerls form string
- * @param str {string}
- * @returns {string[]}
+ * This function return array of vowels from string
  */
 function getVowels(str) {
-  // Write your code here
+  return Array.from(str).filter(c => 'aeiouAEIOU'.includes(c));
 }
 
 /**
  * This function get array of string and concat them into one string
- * @param str {string[]}
- * @returns {string}
  */
-function cancatString() {
-  // Write your code here
+function cancatString(str) {
+  return str.join('');
 }
 
 /**
  * This function split string according to divider
- * @param str {string}
- * @param divider {string}
- * @returns {string[]}
  */
-
 function splitString(str, divider) {
-  // Write your code here
+  return str.split(divider);
 }
 
 /**
  * This function return true if string contains substring
- * @param str {string}
- * @param subStr {string}
- * @returns {boolean}
  */
 function hasSubString(str, subStr) {
-  // Write your code here
+  return str.includes(subStr);
 }
 
 /**
  * This function return true if char is in lower case
- * @param char {string}
- * @returns {boolean}
  */
 function isLowerCase(char) {
-  // Write your code here
+  return char === char.toLowerCase() && char !== char.toUpperCase();
 }
 
 module.exports = {
@@ -147,4 +125,4 @@ module.exports = {
   splitString,
   hasSubString,
   isLowerCase
-}
+};
--- js/lib.spec.js
+++ js/lib.spec.js
@@ -1,9 +1,69 @@
 const lib = require('./lib');
 
-describe('Test suite for testing lib.js', () => {
-  describe('Test suite for testing subtraction function', () => {
-    it('should return subtraction of two numbers', () => {
-      expect(lib.subtraction(5, 1)).toEqual(3);
-    });
+describe('lib.js functions', () => {
+  test('sum works', () => {
+    expect(lib.sum(2, 3)).toBe(5);
+    expect(lib.sum("10", "20")).toBe(30);
+  });
+
+  test('subtraction works', () => {
+    expect(lib.subtraction(10, 3)).toBe(7);
+    expect(lib.subtraction("20", "5")).toBe(15);
+  });
+
+  test('division works', () => {
+    expect(lib.division(10, 2)).toBe(5);
+    expect(() => lib.division(10, 0)).toThrow("Division by zero");
+  });
+
+  test('multiplication works', () => {
+    expect(lib.multiplication(4, 5)).toBe(20);
+  });
+
+  test('percentage works', () => {
+    expect(lib.percentage(200, 10)).toBe(20);
+  });
+
+  test('getWholeNumberPart works', () => {
+    expect(lib.getWholeNumberPart(5.9)).toBe(5);
+  });
+
+  test('getEvenNumbers works', () => {
+    expect(lib.getEvenNumbers([1,2,3,4,5,6])).toEqual([2,4,6]);
+  });
+
+  test('getMaxNumber works', () => {
+    expect(lib.getMaxNumber([1,5,3,9,2])).toBe(9);
+  });
+
+  test('getMinAndMaxNumbers works', () => {
+    expect(lib.getMinAndMaxNumbers([1,5,3,9,2])).toEqual({min:1, max:9});
+  });
+
+  test('arithmeticMeans works', () => {
+    expect(lib.arithmeticMeans([2,4,6])).toBe(4);
+    expect(lib.arithmeticMeans([])).toBe(0);
+  });
+
+  test('getVowels works', () => {
+    expect(lib.getVowels("Hello World")).toEqual(["e","o","o"]);
+  });
+
+  test('cancatString works', () => {
+    expect(lib.cancatString(["Hello"," ","World"])).toBe("Hello World");
+  });
+
+  test('splitString works', () => {
+    expect(lib.splitString("a,b,c", ",")).toEqual(["a","b","c"]);
+  });
+
+  test('hasSubString works', () => {
+    expect(lib.hasSubString("Hello World", "World")).toBe(true);
+    expect(lib.hasSubString("Hello World", "Test")).toBe(false);
+  });
+
+  test('isLowerCase works', () => {
+    expect(lib.isLowerCase("a")).toBe(true);
+    expect(lib.isLowerCase("A")).toBe(false);
   });
 });
